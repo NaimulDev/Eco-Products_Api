f1a5634d72a90f9227763892a519f9cb23175c33
Nimul99 <htnimul99@gmail.com>
2024-05-22 18:13:41 +0600

    product interface implemented

---
+++ src/modules/Product/product.interface.ts
@@ -0,0 +1,22 @@
+// import { Schema, model, connect } from "mongoose";
+
+export type EVariant = {
+  type: string;
+  value: string;
+};
+
+export type EInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+//creteing a EProduct Type
+export type EProduct = {
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: EVariant[];
+  inventory: EInventory;
+};
